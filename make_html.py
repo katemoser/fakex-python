22dc363bb82ff54e9d9d9c0408e109edbc05d34c
Kate Moser <komoser@gmail.com>
2022-09-09 18:50:16 -0500

    got syntax highlighting to work

--- make_html.py
+++ make_html.py
@@ -9,7 +9,7 @@ TEMPLATE = """
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>--- TODO ---</title>
-
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/fontawesome.min.css">
 
@@ -30,7 +30,10 @@ def md_to_html():
 
 
     # turn to html
-    html = markdown.markdown(readme_contents, extensions=["fenced_code"])
+    html = markdown.markdown(
+        readme_contents, 
+        extensions=["fenced_code","codehilite"]
+    )
 
     # insert to template
     page = TEMPLATE.replace("{{content}}", html)
